09df167467ac6efda8c7c62e94f5ca8d32f71d49
szyncel <mtteu1994@gmail.com>
2017-09-21 16:56:25 +0200

    -skromny wygląd

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,17 @@
     var canvas = document.querySelector('#gameCanvas');
     var context = canvas.getContext('2d');
     var ballX = 400;
-    var ballSpeedX = -7;
+    var ballSpeedX = -10;
     var ballY = 150;
-    var ballSpeedY = -3;
+    var ballSpeedY = -5;
     var paddle1Y = 250;
     var paddle2Y = 250;
     var paddleHeight = 100;
 
     var player1Score = 0;
     var player2Score = 0;
-    const WINNING_SCORE = 3;
+    const WINNING_SCORE = 5;
     var showWinScreen = false;
-
 
     function getMousePos(evt) {
         var rect = canvas.getBoundingClientRect();
@@ -23,13 +22,21 @@
         };
     }
 
-
-    var framesPerSecond = 30;
+    var framesPerSecond = 35;
     setInterval(function () {
         moveBall();
         drawGame();
     }, 1000 / framesPerSecond);
 
+
+    canvas.addEventListener('click', function () {
+        if (showWinScreen) {
+            player1Score = 0;
+            player2Score = 0;
+            showWinScreen = false;
+            resetBallPosition();
+        }
+    });
     canvas.addEventListener('mousemove', function (evt) {
         var mousePos = getMousePos(evt);
         paddle1Y = mousePos.y - paddleHeight / 2;
@@ -40,10 +47,12 @@
 
 
     function computerMovment() {
-        if (ballY - 35 > paddle2Y + paddleHeight / 2) paddle2Y += 5;
-        else paddle2Y -= 5;
+        if (ballY - 35 > paddle2Y + paddleHeight / 2) paddle2Y += 10;
+        if ((ballY + 35 < paddle2Y + paddleHeight / 2)) paddle2Y -= 10;
 
     }
+
+
 
     function moveBall() {
         computerMovment();
@@ -57,6 +66,7 @@
             }
             else {
                 player2Score += 1;
+                // alert('Punkt dla przeciwnika');
                 resetBallPosition();
             }
         }
@@ -69,6 +79,7 @@
             }
             else {
                 player1Score += 1;
+                // alert('Punkt dla przeciwnika');
                 resetBallPosition();
             }
         }
@@ -77,44 +88,38 @@
         if (ballY <= 0) ballSpeedY = -ballSpeedY;
     }
 
-    function resetBallPosition() {
+    function resetBallPosition() { //przy wyświetlaniu wyniku gra nadal gra!!
         if (player1Score >= WINNING_SCORE || player2Score >= WINNING_SCORE) {
-            player1Score=0;
-            player2Score=0;
             showWinScreen = true;
         }
-
-        ballSpeedX = -ballSpeedX;
         ballX = canvas.width / 2;
         ballY = canvas.height / 2;
+        ballSpeedX = -ballSpeedX;
     }
-
 
     function drawGame() {
         if (showWinScreen) {
-            colorField("red");
-            context.fillStyle="white";
-            context.font = "40px Impact";
-            context.fillText("Koniec Gry", 300, 50);
-            context.font = "30px Impact";
-            // context.fillText("You: " + player1Score, 100, 150);
-            // context.fillText("Component: " + player2Score, canvas.width - 250, 150);
-            return;
-           
-        } else {
             colorField("black");
-            colorRect(0, paddle1Y, 20, paddleHeight, "red");
-            colorRect(canvas.width - 20, paddle2Y, 20, paddleHeight, "green");
-            drawBall(ballX, ballY, 10, "white");
+            context.fillStyle = "white";
+            context.font = "40px Impact";
+            if (player1Score === WINNING_SCORE) context.fillText("Wygrał gracz nr 1", 360, 100);
+            else context.fillText("Wygrał gracz nr 2", 360, 100);
+            context.font = "20px Impact";
+            context.fillText("Kliknij aby zagrać ponownie", 370, 500);
+            return;
+        } else {
+            colorField("#0e71b4");
+            colorRect(canvas.width/2-1,0,3,canvas.height,"white");
 
+            colorRect(0,canvas.height/2-1,canvas.width,1,"white");
+            colorRect(0, paddle1Y, 20, paddleHeight, "white");
+            colorRect(canvas.width - 20, paddle2Y, 20, paddleHeight, "white");
+            drawBall(ballX, ballY, 10, "white");
+            
             context.font = "28px Impact";
             context.fillText("You: " + player1Score, 100, 40);
             context.fillText("Component: " + player2Score, canvas.width - 250, 40);
         }
-
-
-
-
     }
 
     function colorField(color) {
@@ -134,6 +139,5 @@
         context.arc(leftX, topY, radius, 0, Math.PI * 2, true);
         context.fill();
     }
-
 
 })()
